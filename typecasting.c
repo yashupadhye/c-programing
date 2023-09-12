ff645944998c871f7bfa6e0fa9b1fa070a69fd17
yashupadhye <yashupadhye8390@gmail.com>
2023-09-12 10:43:44 +0530

    main

--- typecasting.c
+++ typecasting.c
@@ -3,27 +3,23 @@ int main()
 {
     // convert the  one data type  to another data type
     // implicit is convert by complier
-   /* int a = 10;
-    int b = 20;
-   float c = a + b;
-    printf("%f\n",c);
-    
-    
+    /* int a = 10;
+     int b = 20;
+    float c = a + b;
+     printf("%f\n",c);
 
-    //explicit type casting
-    float Result=(float)a;
-    printf("%f\n",Result);
-    */
+
+
+     //explicit type casting
+     float Result=(float)a;
+     printf("%f\n",Result);
+     */
 
     // this is used to convert the char data type
-   char cc='A';
-   char cc1='Z';
-   int result=(int)cc;
-   printf("A=%d\n",result);
-   int result1=(int)cc1;
-   printf("Z=%d\n",result1);
 
+    for (int i = 65; i <=90; i++)
+    {
+        printf("%c=%d\n",(char)i,i);
+    }
     
-
 }
-   
