f2063c6fad0a5b45ce1b1bc886f17f3eefee3413
yashupadhye <yashupadhye8390@gmail.com>
2023-09-13 12:02:13 +0530

    main

---
+++ Decision and selection control statements/funcation/acceptnumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void acceptNumber();
+int main()
+{
+    void acceptNumber();
+    return 0;
+}
+void acceptNumber()
+{
+    int num;
+    printf("enter the number:-");
+    scanf("%d",&num);
+}
--- Decision and selection control statements/funcation/frist.c
+++ Decision and selection control statements/funcation/frist.c
@@ -1,5 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+void yash();
 int main()
+
 {
-    
+    yash();
+    yash();
+    yash();
+    yash();
+
+    return 0;
+}
+void yash()
+{
+    int factorical = 1, num;
+    printf("enter the number:-");
+    scanf("%d", &num);
+    for (int i = 1; i <= num; i++)
+    {
+        factorical = factorical * i;
+        printf("factorical=%d\n", factorical);
+    }
 }
