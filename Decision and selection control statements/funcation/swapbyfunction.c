2ed8bd92898251c18e02c5001b45ef518b5c5403
yashupadhye <yashupadhye8390@gmail.com>
2023-09-15 11:09:57 +0530

    main

--- Decision and selection control statements/funcation/swapbyfunction.c
+++ Decision and selection control statements/funcation/swapbyfunction.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
-void swapnumber(int a, int b);
+void swapnumber(int *a, int *b);
 int main()
 {
 
     int a = 10, b = 20;
+    printf("before swapping\n");
     printf("a=%d\n", a);
     printf("b=%d\n", b);
-    swapnumber(a, b);
 
+    swapnumber(&a, &b);
+    printf("after swapping\n");
     printf("a=%d\n", a);
     printf("b=%d\n", b);
+    return 0;
 }
-void swapnumber(int a, int b)
+void swapnumber(int *a, int *b)
 {
 
-    int temp = a;
-    a = b;
-    b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
     printf("swap in the function\n");
-    printf("a=%d\n", a);
-    printf("b=%d\n", b);
+    printf("a=%d\n", *a);
+    printf("b=%d\n", *b);
 }
--- pointer/frist.c
+++ pointer/frist.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    int x = 30;
-    int *ptr;
-    ptr = &x;
-    printf(" value od x is %d\n", x);
-    printf(" address of x is %u\n", &x); //%u is uesed for unsign int u this is use for  or it is use to print address
-    printf(" value of ptr is %d\n", *ptr);
-    printf(" address of ptr is %d\n", &ptr);
-    printf("store address of ptr is %u\n",&ptr);
+    int a = 10;
+    int b = 20;
+    int *ptr1;
+    int *ptr2;
+    ptr1 = &a;
+    ptr2 = &b;
+    printf(" value of a is %d\n", a);
+    printf(" address of a is %u\n", &a); //%u is uesed for unsign int u this is use for  or it is use to print address
+    printf(" value of ptr is %d\n", *ptr1);
+    printf(" address of ptr is %d\n", &ptr1);
+    printf("store address of ptr is %u\n\n\n", &ptr1);
+
+    // value of b
+
+    printf(" value od b is %d\n", b);
+    printf(" address of b is %u\n", &b); 
+    printf(" value of ptr is %d\n", *ptr2);
+    printf(" address of ptr is %d\n", &ptr2);
+    printf("store address of ptr is %u\n", &ptr2);
     return 0;
 }
